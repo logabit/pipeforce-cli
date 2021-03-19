0a2d6ab739e2a227aa53e7ce13a6c7a980dd60d5
sniederm <s.niedermeier@logabit.com>
2021-03-19 14:26:59 +0100

    Python 3 support. New build structure.

--- recipe/recipe.py
+++ recipe/recipe.py
@@ -4,4 +4,4 @@ from buildtool.recipe import DefaultRecipe
 class Recipe(DefaultRecipe):
 
     def pre_build(self):
-        self.ctx.depends_on("pipeforce-commons-java")
+        self.ctx.depends_on(recipe="pipeforce-sdk-java")
