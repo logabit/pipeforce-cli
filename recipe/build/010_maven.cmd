79050501b4f254e2b9f11bcecd24e221c8fb7d05
Juraj <j.homola@logabit.com>
2021-07-13 17:40:19 +0200

    stop command added deploy

--- recipe/build/010_maven.cmd
+++ recipe/build/010_maven.cmd
@@ -1,2 +1,3 @@
 # Build and install jars into local repo
-mvn -f {{ctx.cooked_recipe_home('pom.xml')}} clean package install -Dpipeforce-sdk-java.version={{ctx.tag('pipeforce-sdk-java')}}
+mvn -f {{ctx.cooked_recipe_home('pom.xml')}} clean package install
+
