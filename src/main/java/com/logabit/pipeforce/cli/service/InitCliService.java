854a0b5e334a76c689f44ff97c14dd49d62bc174
sniederm <s.niedermeier@logabit.com>
2023-01-08 10:41:01 +0100

    Init check changed.

--- src/main/java/com/logabit/pipeforce/cli/service/InitCliService.java
+++ src/main/java/com/logabit/pipeforce/cli/service/InitCliService.java
@@ -4,16 +4,37 @@ import com.logabit.pipeforce.cli.BaseCliContextAware;
 import com.logabit.pipeforce.common.util.FileUtil;
 import com.logabit.pipeforce.common.util.PathUtil;
 
+import java.io.File;
+
 /**
  * Service to init given folders as PIPEFORCE app repo folders.
  */
 public class InitCliService extends BaseCliContextAware {
 
+    public static final String FOLDER_NAME_PIPEFORCE = ".pipeforce";
+    public static final String FOLDER_NAME_SRC = "src";
+    public static final String FOLDER_NAME_GLOBAL = "global";
+    public static final String FOLDER_NAME_APP = "app";
+
     public void init(String path) {
 
-        FileUtil.createFolders(PathUtil.path(path, "src", "global", "app"));
-        FileUtil.createFolders(PathUtil.path(path, ".pipeforce"));
+        FileUtil.createFolders(PathUtil.path(path, FOLDER_NAME_SRC, FOLDER_NAME_GLOBAL, FOLDER_NAME_APP));
+        FileUtil.createFolders(PathUtil.path(path, FOLDER_NAME_PIPEFORCE));
         createVSCodeWorkspaceFile(path);
+    }
+
+    /**
+     * Checks if current work dir is inside a valid workspace folder.
+     *
+     * @return
+     */
+    public boolean isWorkDirInsideWorkspace() {
+
+        // Make sure publish is executed always inside init folder for security reasons
+        File workDir = getContext().getCurrentWorkDir();
+        File pipeforceFolder = new File(workDir, ".pipeforce");
+        File srcFolder = new File(workDir, "src");
+        return (pipeforceFolder.exists() && srcFolder.exists());
     }
 
     public void createVSCodeWorkspaceFile(String path) {
